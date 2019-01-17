a4ece46bb2301552d80f8ee97e29d1f3c5115348
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-17 11:32:54 -0800

    i think this is where it goes beyond me. see the forEach loop.

--- main.js
+++ main.js
@@ -4,30 +4,40 @@
 // # START EDITING YOUR JAVASCRIPT HERE
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
 keys.addEventListener('click', e => {
-    if(e.target.matches('button')) {
-        // Do something
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        if(!action) {
+            if(displayedNum === '0') {
+                display.textContent = keyContent;
+            }
+            else {
+                display.textContent = displayedNum + keyContent;
+            }
+            console.log('number key!');
+        }
+        
+        if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            key.classList.add('is-depressed');
+            console.log('operator key!');
+        }
+        
+        if(action === 'decimal') {
+            display.textContent = displayedNum + '.';
+            console.log('decimal key!');
+        }
+        if(action === 'clear') {
+            console.log('clear key!');
+        }
+        if(action === 'calculate') {
+            console.log('equal key!');
+        }
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
     }
 })
-
-const key = e.target;
-const action = key.dataset.action;
-
-if(!action) {
-    console.log('number key!');
-}
-
-if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-    console.log('operator key!');
-}
-
-if(action === 'decimal') {
-    console.log('decimal key!');
-}
-if(action === 'clear') {
-    console.log('clear key!');
-}
-if(action === 'calculate') {
-    console.log('equal key!');
-}
